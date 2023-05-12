1ec49978a8d01f07dce11af167d0ae8fae3b89e3
Ludovico Pestarino <ludovicopestarino00@gmail.com>
2023-05-12 18:42:07 +0200

    main updates

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,25 @@ pub mod peer_conf;
 pub mod server_conf;
 pub mod write_conf;
 
-fn main() {
-    let server_cfg = server_conf::ServerConf::interactive_new();
-    write_conf::save_server_conf(&server_cfg);
+use inquire::*;
+use write_conf::update_server_conf;
 
-    let peer_cfg = peer_conf::PeerConf::interactive_new(server_cfg);
+fn main() {
+    let server_cfg;
+
+    let interface_name = Text::new("Insert interface name").with_default("wg0").prompt().unwrap();
+
+    if std::path::Path::new(&format!("{}.conf", interface_name)).exists() {
+        println!("{}.conf exists, loading it", interface_name);
+        server_cfg = server_conf::load_server_conf(interface_name);
+    }
+    else {
+        println!("{}.conf does not exist, creating it", interface_name);
+        server_cfg = server_conf::ServerConf::interactive_new();
+        write_conf::save_server_conf(&server_cfg);
+    }
+
+    let peer_cfg = peer_conf::PeerConf::interactive_new(&server_cfg);
     write_conf::save_peer_conf(&peer_cfg);
+    update_server_conf(&server_cfg, &peer_cfg);
 }
