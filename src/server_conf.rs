b2eaa996d1b4b3c7ce0a83a60835183c3d7a73ae
Ludovico Pestarino <ludovicopestarino00@gmail.com>
2023-05-12 18:41:53 +0200

    added load config

--- src/server_conf.rs
+++ src/server_conf.rs
@@ -14,6 +14,8 @@ use crate::gen_keys::*;
 
 use inquire::*;
 use rand_core::OsRng;
+use std::{fs::File, io::Read};
+use base64::{engine::general_purpose, Engine as _};
 
 pub struct ServerConf {
     pub interface_name: String,
@@ -25,6 +27,16 @@ pub struct ServerConf {
 impl ServerConf {
     pub fn new(interface_name: String, port: i32) -> ServerConf {
         let priv_key = PrivKey::new(OsRng);
+        let pub_key = PubKey::from(&priv_key);
+        ServerConf {
+            interface_name,
+            priv_key,
+            pub_key,
+            port,
+        }
+    }
+
+    pub fn load(interface_name: String, port: i32, priv_key: PrivKey) -> ServerConf {
         let pub_key = PubKey::from(&priv_key);
         ServerConf {
             interface_name,
@@ -48,3 +60,21 @@ impl ServerConf {
         ServerConf::new(interface_name, port)
     }
 }
+
+pub fn load_server_conf(interface: String) -> ServerConf{
+    let mut file = File::open(format!("{}.conf", interface)).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    let mut lines = contents.lines();
+    lines.next();
+    let priv_key_raw = lines.next().unwrap().split(" = ").collect::<Vec<&str>>()[1];
+    let port = lines.next().unwrap().split(" = ").collect::<Vec<&str>>()[1];
+
+    let priv_key = PrivKey::from(general_purpose::STANDARD.decode(priv_key_raw.as_bytes()).unwrap());
+
+    ServerConf::load(
+        String::from(interface),
+        port.parse::<i32>().unwrap(),
+        priv_key
+    )
+} 
