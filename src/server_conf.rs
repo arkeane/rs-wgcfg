343921c9c136a7efb46d91a600cd78e09c492214
Ludovico Pestarino <ludovicopestarino00@gmail.com>
2023-05-12 17:51:23 +0200

    added interface name field

--- src/server_conf.rs
+++ src/server_conf.rs
@@ -16,16 +16,18 @@ use inquire::*;
 use rand_core::OsRng;
 
 pub struct ServerConf {
+    pub interface_name: String,
     pub priv_key: PrivKey,
     pub pub_key: PubKey,
     pub port: i32,
 }
 
 impl ServerConf {
-    pub fn new(port: i32) -> ServerConf {
+    pub fn new(interface_name: String, port: i32) -> ServerConf {
         let priv_key = PrivKey::new(OsRng);
         let pub_key = PubKey::from(&priv_key);
         ServerConf {
+            interface_name,
             priv_key,
             pub_key,
             port,
@@ -35,7 +37,7 @@ impl ServerConf {
     pub fn interactive_new() -> ServerConf {
         println!("--------------------------------------------------");
         println!("Configure Server: ");
-
+        let interface_name = Text::new("Insert interface name").with_default("wg0").prompt().unwrap();
         let port = CustomType::<i32>::new("Insert port number")
             .with_formatter(&|i| format!("{}", i))
             .with_error_message("please insert an integer number")
@@ -43,6 +45,6 @@ impl ServerConf {
             .prompt()
             .unwrap();
         println!("--------------------------------------------------");
-        ServerConf::new(port)
+        ServerConf::new(interface_name, port)
     }
 }
